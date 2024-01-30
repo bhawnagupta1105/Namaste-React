9342a6d4a0c2e8ced4e72a8451f95c0caa4e8733
bhawnagupta1105 <bhawanagupta303@gmail.com>
2024-01-30 23:56:01 +0530

    Doe

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -9,13 +9,15 @@ const Body = () => {
 
     },[])
 
-    const fetchData = async() => {
-        const data = await fetch();
+    const fetchData = async( ) => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.84401&lng=75.803655&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
 
         const json = await data.json();
 
         console.log(json);
-        setlistofRestaurants(list of api);
+        setlistofRestaurants(json?.data?.card?.card?.gridElements);
 
     }
     return (
