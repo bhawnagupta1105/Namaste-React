edef8542ceebbadc34885b669d7cc3db52030eb7
bhawnagupta1105 <bhawanagupta303@gmail.com>
2024-01-31 18:22:41 +0530

    ok

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,52 +1,86 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
-    const [listofRestaurants,setlistofRestaurants] = useState([resList]);
+  const [listofRestaurants, setlistofRestaurants] = useState([]);
+  const [filteredRestaurant, setfilteredRestaurant] = useState([]);
+  const [searchText, setsearchText] = useState("");
 
-    useEffect(() => {
-        fetchData();
+  // useEffect(() => {
+  //     fetchData();
 
-    },[])
+  // },[])
 
-    const fetchData = async( ) => {
-        const data = await fetch(
-            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.84401&lng=75.803655&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-        );
+  // const fetchData = async( ) => {
+  //     const data = await fetch(
+  //         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.84401&lng=75.803655&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  //     );
 
-        const json = await data.json();
+  //     const json = await data.json();
 
-        console.log(json);
-        setlistofRestaurants(json?.data?.card?.card?.gridElements);
+  //     console.log(json);
+  //     //Optional Chaining
+  //     setlistofRestaurants(json?.data?.card?.card?.gridElements);
+  //     setfilteredRestaurant(json?.data?.card?.card?.gridElements);
 
-    }
-    return (
-      <div className="Body">
-          <div className="Searchbar">
-          <img className="search-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMNRGo7hXsP5ZCE5XLiLHoTiy3D1CJ6IQyorV9WAp3npDxQeZkXUb4fCz3zpPumAxChjY&usqp=CAU"></img>
-              <input  type="text" className="s1" placeholder="Search"></input>
-          </div>
-          <div className="filter">
-              <button className="filter-btn" onClick={() =>{
-                  const filteredList = listofRestaurants.filter(
-                      (res) => res.data.rating >4
-                      );
-                      setlistofRestaurants(filteredList)
-                //filter he restro
+  // };
 
+  // // Conditional Rendering
+  // if(listofRestaurants.length === 0){
+  //     return <Shimmer/>
+  // }
 
-                console.log(resList)
-              }}>Top Rated Restaurants</button>
-          </div>
-          <div className="res-container">
+  //instead of if else we will simply use ternary operator
+  return listofRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="Body">
+      <div className="filter">
+        <div className="Searchbar">
+          {/* <img className="search-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMNRGo7hXsP5ZCE5XLiLHoTiy3D1CJ6IQyorV9WAp3npDxQeZkXUb4fCz3zpPumAxChjY&usqp=CAU"></img> */}
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setsearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            onClick={() => {
+              const filteredRestaurant = listofRestaurants.filter(
+                (res) => res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setfilteredRestaurant(filteredRestaurant)
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listofRestaurants.filter(
+              (res) => res.data.rating > 4
+            );
+            setlistofRestaurants(filteredList);
+            //filter he restro
+
+            console.log(resList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      {/* <div className="res-container">
              
-           {resList.map((restaurant) => (
+           {filteredRestaurant.map((restaurant) => (
             <RestaurantCard key = {restaurant.data.id} resData = {restaurant}/>
            ))}
   
-          </div>
-  
-      </div>
-    );
-  };
-  export default Body;
+          </div> */}
+    </div>
+  );
+};
+export default Body;
--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -1,100 +1,100 @@
-const resList =[
-    {
-      type:"restaurant",
-      data : {
-      id: "0",
-      name: "Subway",
-      rating: 4.7,
-      average_cost: 350,
-      average_time: 41,
-      cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
-      img_url:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
-      veg: false,
-      promoted: true
+// const resList =[
+//     {
+//       type:"restaurant",
+//       data : {
+//       id: "0",
+//       name: "Subway",
+//       rating: 4.7,
+//       average_cost: 350,
+//       average_time: 41,
+//       cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
+//       img_url:
+//         "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
+//       veg: false,
+//       promoted: true
     
-      }
-    },
-    {
-      type:"restaurant",
-      data : {
-      id: "1",
-      name: "Subway",
-      rating: 4.7,
-      average_cost: 350,
-      average_time: 41,
-      cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
-      img_url:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
-      veg: false,
-      promoted: true
+//       }
+//     },
+//     {
+//       type:"restaurant",
+//       data : {
+//       id: "1",
+//       name: "Subway",
+//       rating: 4.7,
+//       average_cost: 350,
+//       average_time: 41,
+//       cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
+//       img_url:
+//         "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
+//       veg: false,
+//       promoted: true
     
-      }
-    },
-    {
-      type:"restaurant",
-      data : {
-      id: "2",
-      name: "Subway",
-      rating: 4.7,
-      average_cost: 350,
-      average_time: 41,
-      cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
-      img_url:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
-      veg: false,
-      promoted: true
+//       }
+//     },
+//     {
+//       type:"restaurant",
+//       data : {
+//       id: "2",
+//       name: "Subway",
+//       rating: 4.7,
+//       average_cost: 350,
+//       average_time: 41,
+//       cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
+//       img_url:
+//         "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
+//       veg: false,
+//       promoted: true
     
-      }
-    },
-    {
-      type:"restaurant",
-      data : {
-      id: "3",
-      name: "Subway",
-      rating: 4.7,
-      average_cost: 350,
-      average_time: 41,
-      cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
-      img_url:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
-      veg: false,
-      promoted: true
+//       }
+//     },
+//     {
+//       type:"restaurant",
+//       data : {
+//       id: "3",
+//       name: "Subway",
+//       rating: 4.7,
+//       average_cost: 350,
+//       average_time: 41,
+//       cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
+//       img_url:
+//         "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
+//       veg: false,
+//       promoted: true
     
-      }
-    },
-    {
-      type:"restaurant",
-      data : {
-      id: "4",
-      name: "Subway",
-      rating: 2,
-      average_cost: 350,
-      average_time: 41,
-      cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
-      img_url:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
-      veg: false,
-      promoted: true
+//       }
+//     },
+//     {
+//       type:"restaurant",
+//       data : {
+//       id: "4",
+//       name: "Subway",
+//       rating: 2,
+//       average_cost: 350,
+//       average_time: 41,
+//       cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
+//       img_url:
+//         "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
+//       veg: false,
+//       promoted: true
     
-      }
-    },
-    {
-      type:"restaurant",
-      data : {
-      id: "5",
-      name: "Subway",
-      rating: 3.7,
-      average_cost: 350,
-      average_time: 41,
-      cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
-      img_url:
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
-      veg: false,
-      promoted: true
+//       }
+//     },
+//     {
+//       type:"restaurant",
+//       data : {
+//       id: "5",
+//       name: "Subway",
+//       rating: 3.7,
+//       average_cost: 350,
+//       average_time: 41,
+//       cuisines: ["Healthy Food", " Salads", " Snacks", " Desserts", " Beverages"],
+//       img_url:
+//         "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/sdipu3xigqafezlopx03",
+//       veg: false,
+//       promoted: true
     
-      }
-    }
-  ];
+//       }
+//     }
+//   ];
 
-  export default resList;
+//   export default resList;
