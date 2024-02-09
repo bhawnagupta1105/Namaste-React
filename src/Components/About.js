f38dd3530476afbea005eb6bd61be47ed7120df3
bhawnagupta1105 <bhawanagupta303@gmail.com>
2024-02-09 15:46:51 +0530

    ok

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,11 @@
+import User from "./User";
 const About = () => {
     return (
         <div>
             <h1>this is about component</h1>
+           <User></User>
         </div>
+        
     );
 };
 
---
+++ src/Components/User.js
@@ -0,0 +1,12 @@
+
+const User = () => {
+    return (
+        <div className="user-card">
+            <h2>Name:Bhawna</h2>
+            <h3>Loation:Jaipur</h3>
+            <h4>Contact:bhawanagupta303@gmail.com</h4>
+
+        </div>
+    );
+};
+export default User;
